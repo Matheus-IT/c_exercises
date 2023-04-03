74d3de4ad7ef2cc45974aad41bd87e772dedbfaf
matheus costa <matheuscosta.tech@gmail.com>
2023-04-02 21:57:43 -0300

    Solving question on beecrowd

---
+++ beecrowd_questions/14question_3055.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    uint16_t a, m;
+
+    cin >> a;
+    cin >> m;
+
+    uint16_t n = 2 * m - a;
+
+    cout << n << endl;
+
+    return 0;
+}
